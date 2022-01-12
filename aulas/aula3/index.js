a72b11325e330c9e60134de015b8391e7da1f1f9
Thiago Vieira <jaumsix6@gmail.com>
2022-01-12 01:22:49 -0300

    att

---
+++ aulas/aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Hello World');
+console.log("Hello World");
+console.log(`Hello World`) 
+
+console.log(123456, 15.53)
---
+++ exercicios/ex001/index.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Thiago Vieira". Estou aprendendo Js às 10 da manha');
